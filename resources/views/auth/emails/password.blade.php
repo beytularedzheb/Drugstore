a0c71cc9d05e4d73d464e841e189454a1bb38abd
Beytula Redzheb <beytularedzheb@gmail.com>
2016-08-25 20:26:17 +0300

    FIxed password reset paths

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url(LaravelLocalization::getCurrentLocale() . 'password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Click here to reset your password: <a href="{{ $link = url(LaravelLocalization::getCurrentLocale() . '/password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
